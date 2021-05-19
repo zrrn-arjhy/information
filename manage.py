892633b774df38b513d4006cc552922eddffa486
zrrn-arjhy <crs7538208683562@163.com>
2021-05-19 18:21:12 +0800

    创建工程配置的类Config

--- manage.py
+++ manage.py
@@ -3,6 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+
+
+
+
+app.config.from_object(Config)
+
+
 @app.route("/")
 def index():
     return 'hello'
