23087aee5e65359113033facc070dfd361d9a1c7
zrrn-arjhy <crs7538208683562@163.com>
2021-05-29 13:28:43 +0800

    创建redis储存对象，并在配置中填写相关配置

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -11,9 +13,17 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
     SQLALCHEMY_TRACK_MODIFICATIONS = Flask
 
+    # redis配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+# 创建redis储存对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+CSRFProtect(app)
 
 
 @app.route("/")
