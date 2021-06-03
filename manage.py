6eae70f004071987bea7e23582653ba25bcd509d
zrrn-arjhy <crs7538208683562@163.com>
2021-06-03 17:55:10 +0800

    利用flask_session扩展，将session数据保存到Redis中

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -17,9 +18,16 @@ class Config(object):
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
 
+    # flask_session的配置信息
+    SESSION_TYPE = 'redis' # 指定session 保存到redis 中
+    SESSION_USE_SIGNER = True # 让　cookie 中的session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT) #使用redis的实例
+    PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期
+
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+Session(app)
 
 # 创建redis储存对象
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
