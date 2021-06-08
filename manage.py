4c06b4676fe1f6e05069cb159f9eb436c8f136ea
zrrn-arjhy <crs7538208683562@163.com>
2021-06-08 17:52:41 +0800

    在与manage.py 同级目录下创建config.py文件，用于项目的配置文件＆在manager.py中引入Config类，直接使用

--- manage.py
+++ manage.py
@@ -1,29 +1,13 @@
 from flask import Flask
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
+from flask_script import Manager
 
 app = Flask(__name__)
-
-
-class Config(object):
-    """工程配置信息"""
-    DEBUG = True
-    # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
-    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
-
-    # redis配置
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # flask_session的配置信息
-    SESSION_TYPE = 'redis' # 指定session 保存到redis 中
-    SESSION_USE_SIGNER = True # 让　cookie 中的session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT) #使用redis的实例
-    PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期
-
+from config import Config
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -32,6 +16,10 @@ Session(app)
 # 创建redis储存对象
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
+
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route("/")
